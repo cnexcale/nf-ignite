7d1785045efb2e367264b2f65b5bf80250dadc5e
cnexcale <36278502+cnexcale@users.noreply.github.com>
2022-11-03 10:57:05 +0100

    added .command.run to script unstaging step

--- src/main/nextflow/executor/IgScriptStagingStrategy.groovy
+++ src/main/nextflow/executor/IgScriptStagingStrategy.groovy
@@ -77,6 +77,7 @@ class IgScriptStagingStrategy extends IgFileStagingStrategy implements ScriptFil
         copyFromScratchToWorkDir(TaskRun.CMD_LOG, true)
         copyFromScratchToWorkDir(TaskRun.CMD_ENV,
                                  super.task.getOutputEnvNames() == null || super.task.getOutputEnvNames().empty)
+        copyFromScratchToWorkDir(TaskRun.CMD_RUN) // only for debugging purposes
 
     }
 
